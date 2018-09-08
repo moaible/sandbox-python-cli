7d6947bfc92bd32b2d24c4de5d9b8f3bedfe2cd2
moaible <moai.motodera@gmail.com>
2018-09-08 09:00:08 +0900

    feat: omit require command option

--- sandboxcli/main.py
+++ sandboxcli/main.py
@@ -4,10 +4,9 @@ import click
 from commands import command
 
 
-@click.command(help='Simple click CLI')  # (1)
-# (2)
-@click.option('-n', '--name', 'name', type=str, help='your name', required=True)
-@click.option('-l', '--last-name', 'last_name', type=str, help='your last name', default='Fujimoto', required=False)
+@click.command(help='Simple click CLI')
+@click.option('-n', '--name', 'name', type=str, help='your name', required=False)
+@click.option('-l', '--last-name', 'last_name', type=str, help='your last name', default='moaible', required=False)
 def main(name, last_name):
     print('Your name is %s' % name)
     ret = command.execute(["ls"])
